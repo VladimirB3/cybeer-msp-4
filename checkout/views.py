0941eb2654c4c9043627e3185f7f2466a8d43276
Vladimirs Boreckis <v.boreckis@gmail.com>
2020-10-24 14:47:08 +0000

    Added stripe elements javascript file in checkout/static

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HfnZjDq9aCFMfjLIBfWHl3HySatXK2M2SjreHi0W4B71NY4xiF6Kh1WOUanVrGW4rKDZgASTooJLwob3EPKDhXa0083qI1Nhf',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
